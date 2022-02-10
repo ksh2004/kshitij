b97b7ea06ac646ecc709218154643b4e07a6b9f6
ksh2004 <99409764+ksh2004@users.noreply.github.com>
2022-02-10 19:23:44 +0530

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,13 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDmGvzo9zMfg3BEe1Pm6LP_jRTKvv-KlwY",
+    authDomain: "virus19-ea550.firebaseapp.com",
+    projectId: "virus19-ea550",
+    storageBucket: "virus19-ea550.appspot.com",
+    messagingSenderId: "166373413396",
+    appId: "1:166373413396:web:56db71eb9451b631548d01"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
